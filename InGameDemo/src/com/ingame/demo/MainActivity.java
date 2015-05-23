91c9dd62b167d5e3e7f8e4a01d6d43e92bb9f444
Dũng Trần <dungtn.dev@gmail.com>
2015-05-23 15:53:56 +0700

    Update Facebook function
    - Share game and tag friend on facebook wall
    - Post message to facebook wall
    - Get facebook friend list
    - Show hot game list

--- InGameDemo/src/com/ingame/demo/MainActivity.java
+++ InGameDemo/src/com/ingame/demo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.ingame.demo;
 
+import java.util.Random;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.content.IntentFilter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -10,9 +16,8 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.ingamesdk.manager.IGReceiver;
+import com.ingamesdk.Listener.IGListenerInterface;
 import com.ingamesdk.manager.InGameSDK;
-import com.ingamesdk.model.IGSession;
 
 public class MainActivity extends Activity {
 
@@ -22,8 +27,6 @@ public class MainActivity extends Activity {
 	public static MainActivity instance;
 	Button btnLogin, btnPayment, btnShowUser;
 	TextView tvInfo;
-	private GameReceiver m_gameReceiver;
-	private IntentFilter filter;
 	private boolean isloggedin = false;
 
 	@Override
@@ -33,53 +36,72 @@ public class MainActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.main);
 		instance = this;
+		
+		Listener listener = new Listener();
 		m_InGameSDK = InGameSDK.getInstance();
-		m_InGameSDK.init(this, true, true, "http://ingame.wc.lt/callbacktest1.php");
+		m_InGameSDK.setListener(listener);
+		m_InGameSDK.init(this, true, true, "");
+		m_InGameSDK.setgameOrderId(random());
 		tvInfo = (TextView) findViewById(R.id.tvInfo);
 		btnLogin = (Button) findViewById(R.id.btnLogin);
 		btnPayment = (Button) findViewById(R.id.btnPayment);
 		btnShowUser = (Button) findViewById(R.id.btnShowUser);
-		m_gameReceiver = new GameReceiver();
-		filter = new IntentFilter();
-		super.onCreate(savedInstanceState);
+		
+		if (!isloggedin)
+			InGameSDK.getInstance().callLogin();
 	}
-
+	
+	public static String random() {
+	    Random generator = new Random();
+	    StringBuilder randomStringBuilder = new StringBuilder();
+	    int randomLength = generator.nextInt(15);
+	    char tempChar;
+	    for (int i = 0; i < randomLength; i++){
+	        tempChar = (char) (generator.nextInt(96) + 32);
+	        randomStringBuilder.append(tempChar);
+	    }
+	    return randomStringBuilder.toString();
+	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		filter.addAction(this.getPackageName() + "ingame.login.success");
-		filter.addAction(this.getPackageName() + "ingame.logout.success");
-		registerReceiver(m_gameReceiver, filter);
-		InGameSDK.getInstance().addSDKButton(this);
-		InGameSDK.getInstance().setContext(this);
+		InGameSDK.getInstance().onResume(this);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		InGameSDK.getInstance().removeSDKButton(this);
+		InGameSDK.getInstance().onPause();
 	}
+	
 
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		unregisterReceiver(m_gameReceiver);
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		InGameSDK.getInstance().onActivityResult(requestCode, resultCode, data);
 	}
-
+	
 	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			moveTaskToBack(true);
-		}
-		return super.onKeyDown(keyCode, event);
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
 	}
-
-	// Alternative variant for API 5 and higher
+	
 	@Override
-	public void onBackPressed() {
-		moveTaskToBack(true);
-	}
+	  public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if (keyCode == KeyEvent.KEYCODE_BACK) {
+	      moveTaskToBack(true);
+	    }
+	    return super.onKeyDown(keyCode, event);
+	  }
+	        
+	  // Alternative variant for API 5 and higher
+	  @Override
+	  public void onBackPressed() {
+	    moveTaskToBack(true);
+	  }
 
 	public void login(View v) {
 		if (!isloggedin)
@@ -97,32 +119,78 @@ public class MainActivity extends Activity {
 	}
 
 	public void makePayment(View v) {
-		System.out.println("GameOrderID: " + System.currentTimeMillis());
-		m_InGameSDK.callPayment(System.currentTimeMillis() + "");
+		m_InGameSDK.callPayment(random());
 	}
+	
+	public void Invite(View v)
+	{
+		m_InGameSDK.callInviteFriend();
+	}
+	
+	public void GetFriend(View v)
+	{
+		m_InGameSDK.callGetFBFriendList();
+	}
+	
+	public void Share(View v)
+	{
+		m_InGameSDK.callShareMessageFromGame("Test", null);
+	}
+	
+	public void MoreGame(View v)
+	{
+		m_InGameSDK.callShowMoreGame();
+	}
+	
+	public class Listener implements IGListenerInterface{
 
-	private class GameReceiver extends IGReceiver {
 		@Override
-		public void onLoginSuccess(IGSession session) {
-			// Toast.makeText(getApplicationContext(),
-			// "Trả ra đăng nhập thành công: " + session.getUserName(),
-			// Toast.LENGTH_SHORT).show();
-			tvInfo.setText("Hello " + session.getUserName());
-			tvInfo.setVisibility(View.VISIBLE);
-			btnLogin.setText("Logout");
-			btnPayment.setVisibility(View.VISIBLE);
-			btnShowUser.setVisibility(View.VISIBLE);
-			isloggedin = true;
+		public void LoginSuccessListener(JSONObject json) {
+			// TODO Auto-generated method stub
+			try {
+				String id = json.getString("userID");
+				String name = json.getString("userName");
+				String accesstoken = json.getString("accessToken");
+				String phone = json.getString("phone");
+				String email = json.getString("email");
+				System.out.println("XXXXXX id = " + id + " name = " + name + " accesstoken = " + accesstoken + " email = " +
+						email + " phone = " + phone);
+				
+				
+				tvInfo.setText("Hello " + name);
+				Log.i("IngameDemo", "XXXXXXXX" + accesstoken + ";" + id);
+				tvInfo.setVisibility(View.VISIBLE);
+				btnLogin.setText("Logout");
+				btnPayment.setVisibility(View.VISIBLE);
+				btnShowUser.setVisibility(View.VISIBLE);
+				isloggedin = true;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
-
+		
 		@Override
-		public void onLogoutSuccess() {
+		public void LogOutSuccessListener() {
+			// TODO Auto-generated method stub
 			tvInfo.setVisibility(View.GONE);
 			btnPayment.setVisibility(View.GONE);
 			btnShowUser.setVisibility(View.GONE);
 			btnLogin.setText("Login");
 			isloggedin = false;
+			System.out.println("XXXX Logged out successfully");
 		}
 
+		@Override
+		public void GetFriendListSuccessListener(JSONObject json) {
+			System.out.println("XXXX GetFriendListSuccessListener == " + json.toString());
+		}
+
+		@Override
+		public void InviteFriendSuccessListener() {
+			// TODO Auto-generated method stub
+			System.out.println("XXXX invite friend success");
+		}
 	}
 }
